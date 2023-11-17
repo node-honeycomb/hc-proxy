08d554999a52168e80620125ec1e6fb173a9d237
yusong.hy <yusong.hy@x.local>
2023-11-17 10:38:20 +0800

    fix: path url join bug

--- test/server/http_server.js
+++ test/server/http_server.js
@@ -29,6 +29,9 @@ exports.start = function startServer(callback) {
   server.on('upgrade', (req, socket, head) => {
     const key = req.headers['sec-websocket-key'];
 
+    if(req.url.indexOf("localhost")>=0) {
+      return socket.write('HTTP/1.1 404'); 
+    }
     if(req.url.startsWith('/service-ws')) {
       if(!req.headers.signature) {
         return socket.write('HTTP/1.1 404');
